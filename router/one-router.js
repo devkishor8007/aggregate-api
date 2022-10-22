33196f630db5b37d781f181a34198b6dd8f6d447
kishorkc <kishor8007kc@gmail.com>
2022-10-22 22:31:24 +0545

    remove unneccessary line

--- router/one-router.js
+++ router/one-router.js
@@ -14,9 +14,3 @@ router.get("/match", matchOne);
 router.get("/pagination", paginationOne);
 
 module.exports = router;
-
-// GET: localhost:5000/  get all
-// GET: localhost:5000/match  get the price is greater than 450
-// POST: localhost:5000/create-dummy  create a dummy list of object
-// POST: localhost:5000/ create a dummy object of name, price & address
-// GET: localhost:5000/pagination?limit=5&skip=0&price=250 
